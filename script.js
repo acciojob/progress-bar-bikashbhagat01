d941d07ea11bffe2672da5b8b746cffa23845d9f
Bikash Bhagat <bikash.bhagat01@gmail.com>
2024-09-17 22:06:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -37,10 +37,6 @@ function update() {
     }
   });
   
-  const actives = document.querySelectorAll('.active');
-  
-  progress.style.width = ((actives.length -1)/(circles.length - 1)) *100 + '%';
-  
   if(currentActive === 1)
   {
     prev.disabled = true;
