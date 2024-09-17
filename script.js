5bb498b7f0b65e19d8a9cf9e5f9ac6ce1e30696c
Bikash Bhagat <bikash.bhagat01@gmail.com>
2024-09-17 22:01:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,47 +6,54 @@ const circles = document.querySelectorAll('.circle');
 let currentActive = 1;
 
 next.addEventListener('click', () => {
-    currentActive++;
-
-    if (currentActive > circles.length) {
-        currentActive = circles.length;
-    }
-
-    update();
+  currentActive++;
+  
+  if(currentActive>circles.length)
+  {
+    currentActive = circles.length;
+  }
+  
+  update();
 });
 
 prev.addEventListener('click', () => {
-    currentActive--;
-
-    if (currentActive < 1) {
-        currentActive = 1;
-    }
-
-    update();
+  currentActive--;
+  
+  if(currentActive<1)
+  {
+    currentActive = 1;
+  }
+  
+  update();
 });
 
 function update() {
-    circles.forEach((circle, idx) => {
-        if (idx < currentActive) {
-            circle.classList.add('active');
-        } else {
-            circle.classList.remove('active');
-        }
-    });
-
-    const actives = document.querySelectorAll('.active');
-
-    progress.style.width = ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
-
-    if (currentActive === 1) {
-        prev.disabled = true;
-    } else {
-        prev.disabled = false;
+  circles.forEach((circle, idx) => {
+    if(idx<currentActive) {
+      circle.classList.add('active');
     }
-
-    if (currentActive === circles.length) {
-        next.disabled = true;
-    } else {
-        next.disabled = false;
+    else {
+      circle.classList.remove('active');
     }
+  });
+  
+  const actives = document.querySelectorAll('.active');
+  
+  progress.style.width = ((actives.length -1)/(circles.length - 1)) *100 + '%';
+  
+  if(currentActive === 1)
+  {
+    prev.disabled = true;
+  }
+  else{
+    prev.disabled = false;
+  }
+  
+  if(currentActive === circles.length)
+  {
+    next.disabled = true;
+  }
+  else{
+    next.disabled = false;
+  }
 }
